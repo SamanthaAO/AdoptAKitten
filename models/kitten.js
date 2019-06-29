86b1aec4be3e64ba8bc875d724103bdc4e3f5a22
SamanthaAO <samanthaaorcutt@gmail.com>
2019-06-28 21:58:32 -0700

    create kitten.js to take orm object and apply it to table kittens by creatign kitten object

--- models/kitten.js
+++ models/kitten.js
@@ -1,9 +1,21 @@
 var orm = require("../config/orm.js");
 
 var kitten = {
-    selectAll: function(callback){},
-    insertOne: function(){},
-    updateOne: function(){}
+    selectAll: function(callback){
+        orm.selectAll("kittens", function(res){
+            callback(res);
+        })
+    },
+    insertOne: function(columns, values, callback){
+        orm.insertOne("kittens", columns, values, function(res){
+            callback(res);
+        })
+    },
+    updateOne: function(columnValues, condition, callback){
+        orm.updateOne("kittens", columnValues, condition, function(res){
+            callback(res);
+        })
+    }
 
 
 
